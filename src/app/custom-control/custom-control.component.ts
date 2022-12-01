cbfd934c56d13c8237b2df2e27beb47f2fec7944
nickgrief <wotniga@gmail.com>
2022-12-01 05:49:07 +0400

    Remove comments

--- src/app/custom-control/custom-control.component.ts
+++ src/app/custom-control/custom-control.component.ts
@@ -22,24 +22,20 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
   ],
 })
 export class CustomControlComponent implements ControlValueAccessor {
+  val = '';
   onChange: any = () => {};
   onTouch: any = () => {};
-  val = ''; // this is the updated value that the class accesses
   set value(val: string) {
-    // this value is updated by programmatic changes if( val !== undefined && this.val !== val){
     this.val = val;
     this.onChange(val);
     this.onTouch(val);
   }
-  // this method sets the value programmatically
   writeValue(value: any) {
     this.value = value;
   }
-  // upon UI element value changes, this method gets triggered
   registerOnChange(fn: any) {
     this.onChange = fn;
   }
-  // upon touching the element, this method gets triggered
   registerOnTouched(fn: any) {
     this.onTouch = fn;
   }
